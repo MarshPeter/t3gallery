ccb8b4a7e4946e0c1eefe0d354da72be2155efbe
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-15 16:57:32 +1000

    database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 function TopNav() {
   return (
-    <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
 
       <div>Sign In</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/88f8ecb5-cc38-4165-9207-c4a8150f86a2-1tcrqd.png",
   "https://utfs.io/f/a375b340-1831-4871-999a-6e2f9c52db68-ho5kq5.png",
   "https://utfs.io/f/cdf37060-78b4-4054-b6b1-932b61bfaf42-7kcux2.png",
   "https://utfs.io/f/9d9c2217-468c-495f-81a4-69a52bb53416-m1knjz.png",
-]
+];
 
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
-    <main className="">
+    <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
